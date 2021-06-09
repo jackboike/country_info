72656c6bd0df56f34d0e60949f31b5d15b57041b
Jack Boike <jackboike@mercadolabs.com>
2021-06-08 23:12:13 -0400

    Fixed apollo client uri

--- src/components/apollo.client.js
+++ src/components/apollo.client.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8080/graphql',
+    uri: 'https://learn-about-countries.herokuapp.com/graphql',
     cache: new InMemoryCache()
 });
 
