425927e6c8640d42629ebe0af36e46ed8b6106a4
Jack Boike <jackboike@mercadolabs.com>
2021-06-08 20:22:26 -0400

    Modified Apollo Client

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+import { graphqlHTTP } from 'express-graphql';
+import schema from './server/schema';
+import * as path from "path";
+import * as bodyParser from "body-parser";
+const cors = require('cors')
+const app = express();
+
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+
+app.use('/graphql/', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}))
+
+app.use(express.static(path.join(__dirname + '/client/build')));
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+// Pass to next layer of middleware
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(8080, () => console.log('Running on server post localhost:8080/graphql'));
+
+
